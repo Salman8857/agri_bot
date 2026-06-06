bbe55387607f4493e9797f1a07c75e61c749f06e
Salman Faras <salmanfarassalman@gmail.com>
2026-06-06 13:02:54 +0530

    Use Gradio app for Hugging Face

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
+# from rag_engine import ask_question
+
+# history = []
+
+# while True:
+#     question = input("Ask your weed question: ")
+
+#     if question.lower() == "exit":
+#         print("Chat stopped")
+#         break
+
+#     answer = ask_question(question, history)
+
+#     print("\nAnswer:")
+#     print(answer)
+
+#     history.append({"role": "user", "content": question})
+#     history.append({"role": "assistant", "content": answer})
+
+
+
+import gradio as gr
 from rag_engine import ask_question
 
-history = []
+def respond(message, history):
+    rag_history = []
 
-while True:
-    question = input("Ask your weed question: ")
+    if history:
+        for item in history:
+            if isinstance(item, dict):
+                rag_history.append(item)
 
-    if question.lower() == "exit":
-        print("Chat stopped")
-        break
+    answer = ask_question(message, rag_history)
+    return answer
 
-    answer = ask_question(question, history)
+demo = gr.ChatInterface(
+    fn=respond,
+    title="Weed Management Assistant",
+    description="Ask questions about weeds from the PDF guide.",
+    examples=[
+        "How to control Canada thistle?",
+        "What are the keys to ID for musk thistle?",
+        "What is the lifecycle of bull thistle?",
+        "Herbicide timing for salt cedar?",
+    ],
+)
 
-    print("\nAnswer:")
-    print(answer)
-
-    history.append({"role": "user", "content": question})
-    history.append({"role": "assistant", "content": answer})
+demo.launch()
