0dab69afa04e78c0085d1c08ed55e48fd6ba1a66
Salman Faras <salmanfarassalman@gmail.com>
2026-06-10 16:17:21 +0530

    Configure app for Render deployment

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from rag_engine import ask_question
 
 app = FastAPI()
 
 class Question(BaseModel):
     question: str
+
+app.add_middleware(
+    CORSMiddleware,allow_origins=["https://your-frontend-name.onrender.com"]
+)
 
 @app.get("/")
 def home():
